242ceb2a26e5000fd101a6af8c6221a1f82a86ae
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-11 10:36:32 -0700

    Add query inputs for calendars

--- src/scripts.js
+++ src/scripts.js
@@ -1,20 +1,27 @@
 import { fetchAll } from "./apiCalls";
+import * as dayjs from 'dayjs'
 
-const selectInput = document.querySelector('#destinationsInput')
+const selectInput = document.querySelector("#destinationsInput");
+const tripStartCalendar = document.querySelector("#tripStartInput");
+const tripEndCalendar = document.querySelector("#tripEndInput");
 
-fetchAll().then(data => {
-  console.log(data[2].destinations)
-  onLoadData(data)
-})
+
+fetchAll().then((data) => {
+  console.log(data);
+  onLoadData(data);
+});
 
 function onLoadData(data) {
-  updateSelectOptions(data[2].destinations)
+  updateSelectOptions(data[2].destinations);
 }
 
 function updateSelectOptions(destinations) {
-  selectInput.innerHTML = ''
-  destinations.sort((a, b) => a.destination.localeCompare(b.destination)).forEach(destination => {
-    console.log(destination)
-    selectInput.innerHTML += `<option value=${destination.destination}>${destination.destination}</option>`
-  });
+  selectInput.innerHTML = "";
+  destinations
+    .sort((a, b) => a.destination.localeCompare(b.destination))
+    .forEach((destination) => {
+      console.log(destination);
+      selectInput.innerHTML += `<option value=${destination.destination}>${destination.destination}</option>`;
+    });
 }
+
