f503c96ec0a62a503a2c437df341b09c985f5084
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-11 09:40:08 -0700

    Add fetchAll function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,15 @@
 const fetchData = (url, obj) => {
-  return fetch(url, obj).then(response => {
+  return fetch(url, obj).then((response) => {
     // test response: !response.ok or response.status > 300
-    return response.json()
-  })
-}
+    return response.json();
+  });
+};
+
+const fetchAll = () =>
+  Promise.all([
+    fetchData("http://localhost:3001/api/v1/travelers"),
+    fetchData("http://localhost:3001/api/v1/trips"),
+    fetchData("http://localhost:3001/api/v1/destinations"),
+  ]);
+
+export { fetchAll };
