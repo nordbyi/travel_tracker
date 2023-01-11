18b32cc88b1050d56e83d2c5fd10bcaa6e8854df
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-11 11:19:31 -0700

    Replace updateCalendars with updateStartCalendar and updateEndCalendar

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,19 @@
 import { fetchAll } from "./apiCalls";
-import * as dayjs from 'dayjs'
+import * as dayjs from "dayjs";
 
 const selectInput = document.querySelector("#destinationsInput");
 const tripStartCalendar = document.querySelector("#tripStartInput");
 const tripEndCalendar = document.querySelector("#tripEndInput");
 
-let currentDate = dayjs().format('YYYY/MM/DD')
-console.log(currentDate)
+tripStartCalendar.addEventListener("change", updateEndCalendar);
+
+let currentDate = dayjs().format("YYYY/MM/DD");
 
 fetchAll().then((data) => {
   console.log(data);
   onLoadData(data);
-  updateCalendars()
+  updateStartCalendar()
+  updateEndCalendar()
 });
 
 function onLoadData(data) {
@@ -23,16 +25,21 @@ function updateSelectOptions(destinations) {
   destinations
     .sort((a, b) => a.destination.localeCompare(b.destination))
     .forEach((destination) => {
-      console.log(destination);
       selectInput.innerHTML += `<option value=${destination.destination}>${destination.destination}</option>`;
     });
 }
 
-function updateCalendars() {
-  const calendarDate = dayjs(currentDate).format('YYYY-MM-DD')
-  console.log(tripStartCalendar)
-  tripStartCalendar.setAttribute('min', calendarDate)
-  tripStartCalendar.setAttribute('value', calendarDate)
-  tripEndCalendar.setAttribute('min', tripStartCalendar.value)
-  tripEndCalendar.setAttribute('value', tripStartCalendar.value)
+function updateStartCalendar() {
+  const calendarDate = dayjs(currentDate).format("YYYY-MM-DD");
+  if(!tripStartCalendar.value) {
+    tripStartCalendar.setAttribute("min", calendarDate);
+    tripStartCalendar.setAttribute("value", calendarDate);
+  }
+}
+
+function updateEndCalendar() {
+  if (!tripEndCalendar.value || dayjs(tripStartCalendar.value).isAfter(dayjs(tripEndCalendar.value))) {
+    tripEndCalendar.value = tripStartCalendar.value
+    tripEndCalendar.setAttribute("min", tripStartCalendar.value);
+  }
 }
