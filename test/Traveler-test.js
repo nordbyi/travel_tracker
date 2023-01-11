d6d92c3aaa2a0c6d1449768bc991f7b797dfe6fd
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-11 16:12:20 -0700

    Add ID test

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,17 +1,30 @@
 import { expect } from "chai";
 
-import Traveler from '../src/Traveler'
+import Traveler from "../src/Traveler";
 
-import travelerData from '../src/data/traveler-data'
+import travelerData from "../src/data/traveler-data";
 
-describe('Traveler', function() {
+describe("Traveler", function () {
   let traveler;
 
   beforeEach(() => {
-    traveler = new Traveler();
+    traveler = new Traveler({
+      id: 1,
+      userID: 35,
+      destinationID: 25,
+      travelers: 5,
+      date: "2022/10/04",
+      duration: 18,
+      status: "approved",
+      suggestedActivities: [],
+    });
   });
 
-  it('should return true', function() {
+  it("should return true", () => {
     expect(traveler).to.be.an.instanceof(Traveler);
+  });
+
+  it("Should have an ID", () => {
+    expect(traveler.id).to.equal(1);
   });
 });
