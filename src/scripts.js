488e6be33c7d88c0bf15b41015f0c5b57acfb343
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-11 09:47:20 -0700

    Add onLoadData function

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,11 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+import { fetchAll } from "./apiCalls";
 
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+const selectInput = document.querySelector('#destinationsInput')
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+fetchAll().then(data => {
+  onLoadData()
+})
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
+function onLoadData() {
+  updateSelectOptions()
+}
