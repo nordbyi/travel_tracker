ac445fa0edde9e4a0fd2de7647528b92e6877c67
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-11 10:07:16 -0700

    Add updateSelectInputs function

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,18 @@ import { fetchAll } from "./apiCalls";
 const selectInput = document.querySelector('#destinationsInput')
 
 fetchAll().then(data => {
-  onLoadData()
+  console.log(data[2].destinations)
+  onLoadData(data)
 })
 
-function onLoadData() {
-  updateSelectOptions()
+function onLoadData(data) {
+  updateSelectOptions(data[2].destinations)
+}
+
+function updateSelectOptions(destinations) {
+  selectInput.innerHTML = ''
+  destinations.forEach(destination => {
+    console.log(destination)
+    selectInput.innerHTML += `<option value=${destination.destination}>${destination.destination}</option>`
+  });
 }
