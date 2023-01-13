056a4990387eba1ad1099f827d0a16a768bbb3fb
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-13 16:30:51 -0700

    Add imports for test data

--- test/Trips-test.js
+++ test/Trips-test.js
@@ -1,15 +1,16 @@
 import { expect } from "chai";
+import Trips from "../src/Trips";
+import { tripsData } from "../src/data/tripsData";
+import { destinationsData } from "../src/data/destinationsData";
 
 describe("Traveler", function () {
   let trips;
-  let destinationsData;
-  let tripsData;
 
   beforeEach(() => {
-    trips = new Trip(tripsData, destinationsData);
+    trips = new Trips(tripsData, destinationsData);
   });
 
   it("should be an instance of Trips", () => {
-    expect(user).to.be.an.instanceof(user);
+    expect(trips).to.be.an.instanceof(Trips);
   });
 });
