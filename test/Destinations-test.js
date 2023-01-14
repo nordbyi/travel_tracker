8623f6e78fa5125bb2986c29e5c6bc88e827cadd
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-13 19:42:59 -0700

    Add findByID method test

--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -6,7 +6,7 @@ describe("Traveler", function () {
   let destinations;
 
   beforeEach(() => {
-    destinations = new Trips(destinationsData);
+    destinations = new Destinations(destinationsData);
   });
 
   it("should be an instance of Destinations", () => {
@@ -15,5 +15,17 @@ describe("Traveler", function () {
 
   it("should have an array of trips", () => {
     expect(destinations.destinations).to.deep.equal(destinationsData);
+  });
+
+  it("Should be able to find a destination by ID", () => {
+    expect(destinations.findByID(1)).to.deep.equal({
+      id: 1,
+      destination: "Lima, Peru",
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image:
+        "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      alt: "overview of city buildings with a clear sky",
+    });
   });
 });
