8f495116ef0bcff6ec6aa1ddfd2eae15e01fecd2
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-13 19:44:46 -0700

    Add findByID method

--- src/Destinations.js
+++ src/Destinations.js
@@ -2,6 +2,10 @@ class Destinations {
   constructor(data) {
     this.destinations = data
   }
+
+  findByID(id) {
+    return this.destinations.find(el => el.id === id)
+  }
 }
 
 export default Destinations
