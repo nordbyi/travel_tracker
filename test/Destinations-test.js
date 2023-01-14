285e74b999b8a6d2d245e8012fcef3dd67b0ecd5
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-13 20:15:04 -0700

    Add filterByQuery method test

--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -18,7 +18,7 @@ describe("Destination", function () {
   });
 
   it("Should be able to find a destination by ID", () => {
-    expect(destinations.findByQuery('id', 1)).to.deep.equal({
+    expect(destinations.findByQuery("id", 1)).to.deep.equal({
       id: 1,
       destination: "Lima, Peru",
       estimatedLodgingCostPerDay: 70,
@@ -27,7 +27,7 @@ describe("Destination", function () {
         "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
       alt: "overview of city buildings with a clear sky",
     });
-    expect(destinations.findByQuery('id', 5)).to.deep.equal({
+    expect(destinations.findByQuery("id", 5)).to.deep.equal({
       id: 5,
       destination: "Madrid, Spain",
       estimatedLodgingCostPerDay: 150,
@@ -39,7 +39,9 @@ describe("Destination", function () {
   });
 
   it("Should be able to find a destination by destination name", () => {
-    expect(destinations.findByQuery('destination', "Tokyo, Japan")).to.deep.equal({
+    expect(
+      destinations.findByQuery("destination", "Tokyo, Japan")
+    ).to.deep.equal({
       id: 8,
       destination: "Tokyo, Japan",
       estimatedLodgingCostPerDay: 125,
@@ -48,7 +50,9 @@ describe("Destination", function () {
         "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1971&q=80",
       alt: "city with people walking in crosswalk and brightly lit shops at night",
     });
-    expect(destinations.findByQuery('destination', "Paris, France")).to.deep.equal({
+    expect(
+      destinations.findByQuery("destination", "Paris, France")
+    ).to.deep.equal({
       id: 7,
       destination: "Paris, France",
       estimatedLodgingCostPerDay: 100,
@@ -76,6 +80,39 @@ describe("Destination", function () {
     expect(destinations.findByQuery("destination", true)).to.be.undefined;
     expect(destinations.findByQuery("destination", null)).to.be.undefined;
     expect(destinations.findByQuery("destination", undefined)).to.be.undefined;
-    expect(destinations.findByQuery("destination", "Washington DC, USA")).to.be.undefined;
+    expect(destinations.findByQuery("destination", "Washington DC, USA")).to.be
+      .undefined;
+  });
+
+  it("Should be able find an array of destinations from an array of ids", () => {
+    expect(destinations.filterByQuery("id", [2, 3, 4])).to.deep.equal([
+      {
+        id: 2,
+        destination: "Stockholm, Sweden",
+        estimatedLodgingCostPerDay: 100,
+        estimatedFlightCostPerPerson: 780,
+        image:
+          "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        alt: "city with boats on the water during the day time",
+      },
+      {
+        id: 3,
+        destination: "Sydney, Austrailia",
+        estimatedLodgingCostPerDay: 130,
+        estimatedFlightCostPerPerson: 950,
+        image:
+          "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        alt: "opera house and city buildings on the water with boats",
+      },
+      {
+        id: 4,
+        destination: "Cartagena, Colombia",
+        estimatedLodgingCostPerDay: 65,
+        estimatedFlightCostPerPerson: 350,
+        image:
+          "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        alt: "boats at a dock during the day time",
+      },
+    ]);
   });
 });
