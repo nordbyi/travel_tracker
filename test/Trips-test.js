95943ebdfa6fcd3fd63a8b9b9eb3cb95d70d5928
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-14 20:23:49 -0700

    Add findTripByID test

--- test/Trips-test.js
+++ test/Trips-test.js
@@ -16,4 +16,17 @@ describe("Trips", function () {
   it("should have an array of trips", () => {
     expect(trips.trips).to.deep.equal(tripsData);
   });
+
+  it("Should be able to find a trip by a trip by ID", () => {
+    expect(trips.findTripByID(16)).to.deep.equal({
+      id: 16,
+      userID: 19,
+      destinationID: 27,
+      travelers: 1,
+      date: "2022/11/20",
+      duration: 9,
+      status: "approved",
+      suggestedActivities: [],
+    });
+  });
 });
