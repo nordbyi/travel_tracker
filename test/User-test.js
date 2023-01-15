8d7c0645b7c7fb1fa55071ce11c424edf8613406
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-15 11:07:31 -0700

    Add pastTrips method test npm test

--- test/User-test.js
+++ test/User-test.js
@@ -10,7 +10,7 @@ describe("Traveler", function () {
   let user;
   let destinations;
   let trips;
-  let currentDate
+  let currentDate;
 
   beforeEach(() => {
     user = new User({
@@ -19,7 +19,7 @@ describe("Traveler", function () {
     });
     destinations = new Destinations(destinationsData);
     trips = new Trips(tripsData);
-    currentDate = "2022/01/14"
+    currentDate = "2022/01/14";
   });
 
   it("should be an instance of User", () => {
@@ -42,44 +42,18 @@ describe("Traveler", function () {
     expect(user.name).to.equal("Ham Leadbeater");
   });
 
-  it("Should return it's trips by status and time", () => {
-    expect(user.tripsByStatus(currentDate)).to.deep.equal({
-      past: [
-        {
-          id: 2,
-          userID: 1,
-          destinationID: 22,
-          travelers: 4,
-          date: "2021/05/22",
-          duration: 17,
-          status: "approved",
-          suggestedActivities: [],
-        },
-      ],
-      upcomming: [
-        {
-          id: 11,
-          userID: 1,
-          destinationID: 5,
-          travelers: 4,
-          date: "2022/10/14",
-          duration: 4,
-          status: "approved",
-          suggestedActivities: [],
-        },
-      ],
-      pending: [
-        {
-          id: 14,
-          userID: 1,
-          destinationID: 35,
-          travelers: 1,
-          date: "2023/09/24",
-          duration: 10,
-          status: "pending",
-          suggestedActivities: [],
-        },
-      ],
-    });
+  it("Should return it's past trips", () => {
+    expect(user.pastTrips()).to.deep.equal([
+      {
+        id: 2,
+        userID: 1,
+        destinationID: 22,
+        travelers: 4,
+        date: "2021/05/22",
+        duration: 17,
+        status: "approved",
+        suggestedActivities: [],
+      },
+    ]);
   });
 });
