a44f7200d036158a3e0184191bc78a5666e172ec
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-14 20:36:01 -0700

    Add filterByQuery method test

--- test/Trips-test.js
+++ test/Trips-test.js
@@ -41,13 +41,38 @@ describe("Trips", function () {
   });
 
   it("Should return undefined if no id found", () => {
-    expect(trips.findTripByID(3)).to.be.undefined
-    expect(trips.findTripByID(50)).to.be.undefined
-    expect(trips.findTripByID('1')).to.be.undefined
-    expect(trips.findTripByID(null)).to.be.undefined
-    expect(trips.findTripByID(undefined)).to.be.undefined
-    expect(trips.findTripByID(true)).to.be.undefined
-    expect(trips.findTripByID(false)).to.be.undefined
-    expect(trips.findTripByID([])).to.be.undefined
-  })
+    expect(trips.findTripByID(3)).to.be.undefined;
+    expect(trips.findTripByID(50)).to.be.undefined;
+    expect(trips.findTripByID("1")).to.be.undefined;
+    expect(trips.findTripByID(null)).to.be.undefined;
+    expect(trips.findTripByID(undefined)).to.be.undefined;
+    expect(trips.findTripByID(true)).to.be.undefined;
+    expect(trips.findTripByID(false)).to.be.undefined;
+    expect(trips.findTripByID([])).to.be.undefined;
+  });
+
+  it("Should be able to find all trips for a user ID", () => {
+    expect(trips.filterByQuery("userID", 50)).to.deep.equal([
+      {
+        id: 11,
+        userID: 50,
+        destinationID: 5,
+        travelers: 4,
+        date: "2022/10/14",
+        duration: 4,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 15,
+        userID: 50,
+        destinationID: 13,
+        travelers: 3,
+        date: "2022/07/04",
+        duration: 6,
+        status: "approved",
+        suggestedActivities: [],
+      },
+    ]);
+  });
 });
