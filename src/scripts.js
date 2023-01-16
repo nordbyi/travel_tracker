1ebe99cc70ff924e82f285f09c2850ead554ae09
Ian Nordby <ian.c.nordby@gmail.com>
2023-01-15 17:17:03 -0700

    Add class imports

--- src/scripts.js
+++ src/scripts.js
@@ -1,45 +1,49 @@
-import "swiper/css"
-import "swiper/css/effect-coverflow"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./css/styles.css";
 import { fetchAll } from "./apiCalls";
 import * as dayjs from "dayjs";
 import { swiper, insertSlides } from "./swiper";
 import User from "./User";
+import Destinations from "./Destinations";
+import Trips from "./Trips";
 
 const selectInput = document.querySelector("#destinationsInput");
 const tripStartCalendar = document.querySelector("#tripStartInput");
 const tripEndCalendar = document.querySelector("#tripEndInput");
+const totalExpenses = document.querySelector("");
 
 tripStartCalendar.addEventListener("change", updateEndCalendar);
 
 let currentDate = dayjs().format("YYYY/MM/DD");
-let travelers
-let trips
-let destinations
-let user
+let travelers;
+let trips;
+let destinations;
+let user;
 
 // change fetch all argument to login page traveler id
 fetchAll(1).then((data) => {
   console.log(data);
   onLoadData(data);
-  renderDOM()
+  renderDOM();
 });
 
 function onLoadData(data) {
-  travelers = data[0].travelers
-  trips = data[1].trips
-  destinations = data[2].destinations
-  user = new User(data[3])
+  travelers = data[0].travelers;
+  trips = new Trips(data[1].trips);
+  destinations = new Destinations(data[2].destinations);
+  user = new User(data[3]);
 }
 
 function renderDOM() {
   updateSelectOptions(destinations);
-  updateStartCalendar()
-  updateEndCalendar()
-  insertSlides(destinations)
-  swiper()
+  updateStartCalendar();
+  updateEndCalendar();
+  insertSlides(destinations);
+  swiper();
+  displayTotalExpenses();
 }
 
 function updateSelectOptions(destinations) {
@@ -53,18 +57,22 @@ function updateSelectOptions(destinations) {
 
 function updateStartCalendar() {
   const calendarDate = dayjs(currentDate).format("YYYY-MM-DD");
-  if(!tripStartCalendar.value) {
+  if (!tripStartCalendar.value) {
     tripStartCalendar.setAttribute("min", calendarDate);
     tripStartCalendar.value = calendarDate;
   }
 }
 
 function updateEndCalendar() {
-  if (!tripEndCalendar.value || dayjs(tripStartCalendar.value).isAfter(dayjs(tripEndCalendar.value))) {
-    tripEndCalendar.value = tripStartCalendar.value
+  if (
+    !tripEndCalendar.value ||
+    dayjs(tripStartCalendar.value).isAfter(dayjs(tripEndCalendar.value))
+  ) {
+    tripEndCalendar.value = tripStartCalendar.value;
     tripEndCalendar.setAttribute("min", tripStartCalendar.value);
-  }
-  else {
+  } else {
     tripEndCalendar.setAttribute("min", tripStartCalendar.value);
   }
 }
+
+function displayTotalExpenses() {}
